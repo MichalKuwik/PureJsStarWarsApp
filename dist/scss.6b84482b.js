fe2be9bd55b6abdf9a57c31a39956a8f1335c021
Michał Kuwik <Michal7074@poczta.fm>
2019-12-16 18:46:40 +0100

    add music to the app,doawnload and add audio tag

--- dist/scss.6b84482b.js
+++ dist/scss.6b84482b.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel/src/builtins/css-loader.js"}],"../node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\img\\1.jpg":[["1.dc197a9a.jpg","img/1.jpg"],"img/1.jpg"],"_css_loader":"../node_modules/parcel/src/builtins/css-loader.js"}],"../node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49421" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49436" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
