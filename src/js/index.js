875b4ecfe464a919056de72c68246b83d787f1d4
Michał Kuwik <Michal7074@poczta.fm>
2019-12-17 23:17:24 +0100

    insall gsap using npm, and import TimelineMax in .js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import {selectFunction,searchOption} from './func_select';
 import {searchFormFn} from './func_searchForm';
+import { TweenMax, TimelineMax}  from 'gsap/all';
 
 searchOption;
 //select option function
@@ -7,7 +8,11 @@ selectFunction();
 //searchFrom function
 searchFormFn;
 
+const text = document.querySelector('.header-desc');
 
+const tl = new TimelineMax();
+
+tl.from(text,1,{opacity:0,delay:2})
 
 
 
