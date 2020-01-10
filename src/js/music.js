34706e6207062ee677b4ddfa04c7ed13d3449f6e
Michał Kuwik <Michal7074@poczta.fm>
2020-01-10 14:57:36 +0100

    add change of sound's icon,add display of sound's value

--- src/js/music.js
+++ src/js/music.js
@@ -2,12 +2,12 @@ import startWarsmp3 from '../mp3/sound.mp3';
 import play_arrow from '../img/play-arrow.png';
 import pause_arrow from '../img/pause-arrow.png';
 
-// const volueLevel = document.querySelector('.volume-level');
-// const mutedSvg = document.getElementById('muted');
-// const svg = document.getElementById('svg');
-// mutedSvg.style.display = "none";
-// mutedSvg.style.width = "30px";
-// mutedSvg.style.height = "30px";
+const volueLevel = document.querySelector('.volume-level');
+const mutedSvg = document.getElementById('muted');
+const svg = document.getElementById('svg');
+mutedSvg.style.display = "none";
+mutedSvg.style.width = "30px";
+mutedSvg.style.height = "30px";
 
 export const initAudioPlayer = () => {
   const audio = new Audio();
@@ -35,21 +35,21 @@ export const initAudioPlayer = () => {
     if(audio.paused){
       audio.play();
       changeImgInBtn(play_arrow);
-      // mutedSvg.style.display = 'none'
-      // svg.style.display = 'block';
+      mutedSvg.style.display = 'none';
+      svg.style.display = 'block';
     } else {
       audio.pause();
       changeImgInBtn(pause_arrow);
-      // mutedSvg.style.display = 'block'
-      // svg.style.display = 'none';
+      mutedSvg.style.display = 'block'
+      svg.style.display = 'none';
       console.log('end')
     }
   }
   function setvolume(){
     audio.volume = volumeSlider.value / 100;
     console.log(volumeSlider.value);
-    //A volueLevel.textContent = volumeSlider.value;
-    //A volueLevel.style.color = 'red';
+    volueLevel.textContent = volumeSlider.value;
+    volueLevel.style.color = 'red';
   }
 }
 
