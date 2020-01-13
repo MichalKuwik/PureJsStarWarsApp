0750b537372106066b345b9645c12c6c185b2262
Michał Kuwik <Michal7074@poczta.fm>
2020-01-13 11:06:02 +0100

    add props allow to audo tag

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -12919,7 +12919,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50207" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50115" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
